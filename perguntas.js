64f81e4960883ac35cd002fbf05a3406b94e2a3d
luizaanaaaa <ana.luiza.santos01@escola.pr.gov.br>
2024-11-14 07:40:09 -0300

    2.0

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'O que são linfócitos?',
     'Principais células do sistema imunológico'
 )
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'Quais são os tipos de linfócitos?',
     'T,B e NK'
 )
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'Quais são os representantes do reino Monera?',
     'Bactérias, cianobactérias e arqueobactérias.'
 )
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'Qual é a função das veias?',
     'Transportar sangue do corpo para o coração.'
 )
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'Qual é a função das artérias?',
     'Transportar sangue do coração para o corpo.'
 )
 criaCartao(
-    'BIOLOGIA'
+    'BIOLOGIA',
     'Qual é a função do sangue?',
     'Transportar nutrientes e oxigênio para o corpo.'
 )
